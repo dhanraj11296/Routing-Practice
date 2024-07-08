7fba08906fb0407f783999537e14ef2cb2721fde
dhanraj11296 <rvsanjayraj@gmail.com>
2024-07-08 12:06:59 +0000

    Solution

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+const About = () => (
+  <div className="aboutContainer">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="aboutImg"
+    />
+    <h1 className="aboutHeading">About</h1>
+    <p className="aboutPara">I love to create! I am a frontend web developer</p>
+  </div>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const Home = () => (
+  <div className="homeContainer">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="homeImg"
+    />
+    <h1 className="homeHeading">Home</h1>
+  </div>
+)
+export default Home
